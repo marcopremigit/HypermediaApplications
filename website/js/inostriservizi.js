9a0b827bf319a4795362ae23c84a3fb5a5b98f51
Unknown <fabrizio.siciliano@mail.polimi.it>
2020-04-17 23:30:09 +0200

    [CSS] - Refactored html and css for footer and header csses to use

--- website/js/inostriservizi.js
+++ website/js/inostriservizi.js
@@ -1,16 +1,14 @@
 window.onload = () => {
-    Breadcrumbs.loadCrumbs([
-        {
-            page: "homepage.html",
-            title: "Home"
-        },
-        {
-            page: "inostriservizi.html",
-            title: "I nostri servizi"
-        }
-    ]);
-    let form = document.getElementById("contact-form");
-    
+  Breadcrumbs.loadCrumbs([
+      {
+          page: "homepage.html",
+          title: "Home"
+      },
+      {
+          page: "inostriservizi.html",
+          title: "I nostri servizi"
+      }
+  ]);
 }
 
 //html card class
@@ -25,31 +23,26 @@ class Card {
     this.BS.image     = document.createElement('img');
     this.BS.info      = document.createElement('div');
     this.BS.title     = document.createElement('h4');
-    this.BS.link = document.createElement('a');
+    this.BS.link      = document.createElement('a');
 
     this.BS.card.appendChild(this.BS.link);
     this.BS.link.appendChild(this.BS.image);
-    this.BS.info.appendChild( this.BS.title);  
-    this.BS.card.appendChild( this.BS.info );
-    this.BS.container.appendChild( this.BS.card );
+    this.BS.info.appendChild(this.BS.title);  
+    this.BS.card.appendChild(this.BS.info);
+    this.BS.container.appendChild(this.BS.card);
 
     this.BS.container.className = 'col-4 mb-3';
     this.BS.card.className      = 'card';
     this.BS.image.className     = 'card-img-top';
     this.BS.title.className     = 'card-title';
-    
-
   }
 
-
-  add ( service_name, image, page_link)
-  {
+  add ( service_name, image, page_link){
     this.BS.image.src = image;
     this.BS.title.textContent = service_name;
     this.BS.link.href = page_link;
     let newNode = this.BS.container.cloneNode(true);
-    this.hmi_ref.appendChild( newNode );
-
+    this.hmi_ref.appendChild(newNode);
   }
 }
 

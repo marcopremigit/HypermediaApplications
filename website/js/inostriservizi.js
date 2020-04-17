5a5005b68c15dc2eed7d9c825ab1db7c98cf797f
Marco <marco.premi@mail.polimi.it>
2020-04-17 21:42:32 +0200

    [Servizi] - Card generation with links images

--- website/js/inostriservizi.js
+++ website/js/inostriservizi.js
@@ -23,28 +23,32 @@ class Card {
     this.BS.image     = document.createElement('img');
     this.BS.info      = document.createElement('div');
     this.BS.title     = document.createElement('h4');
-    this.BS.info.appendChild( this.BS.title )
-  
-    this.BS.card.appendChild( this.BS.image );
+    this.BS.link = document.createElement('a');
+
+    this.BS.card.appendChild(this.BS.link);
+    this.BS.link.appendChild(this.BS.image);
+    this.BS.info.appendChild( this.BS.title);  
     this.BS.card.appendChild( this.BS.info );
     this.BS.container.appendChild( this.BS.card );
+
+    
+
 
     this.BS.container.className = 'col-md-3';
     this.BS.card.className      = 'card';
     this.BS.image.className     = 'card-img-top';
     this.BS.title.className     = 'card-title';
     
-   
-
 
   }
 
 
-  add ( service_name,image)
+  add ( service_name, image, page_link)
   {
     this.BS.image.src = image;
     this.BS.title.textContent = service_name;
-    let newNode = this.BS.container.cloneNode(true)
+    this.BS.link.href = page_link;
+    let newNode = this.BS.container.cloneNode(true);
     this.hmi_ref.appendChild( newNode );
 
   }
@@ -54,8 +58,8 @@ class Card {
 //In inostriservizi.html we have <div id="card-space" class="row"> </div> --> the row where che cards are inserted
 const CardSpace = document.getElementById('card-space');
 let myCard = new Card( CardSpace );
-myCard.add('Ripetizioni matematica','https://source.unsplash.com/random/1920x1080');
-myCard.add('Ripetizioni italiano','https://source.unsplash.com/random/1920x1080');
-myCard.add('Ripetizioni storia','https://source.unsplash.com/random/1920x1080');
-myCard.add('Ripetizioni geografia','https://source.unsplash.com/random/1920x1080', );
-myCard.add('Ripetizioni scienze','https://source.unsplash.com/random/1920x1080');
+myCard.add('Ripetizioni matematica','https://source.unsplash.com/random/1920x1080','https://source.unsplash.com/random/1920x1080' );
+myCard.add('Ripetizioni italiano','https://source.unsplash.com/random/1920x1080', 'https://source.unsplash.com/random/1920x1080');
+myCard.add('Ripetizioni storia','https://source.unsplash.com/random/1920x1080', 'https://source.unsplash.com/random/1920x1080');
+myCard.add('Ripetizioni geografia','https://source.unsplash.com/random/1920x1080', 'https://source.unsplash.com/random/1920x1080');
+myCard.add('Ripetizioni scienze','https://source.unsplash.com/random/1920x1080', 'https://source.unsplash.com/random/1920x1080');

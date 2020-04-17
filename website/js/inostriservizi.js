8054d94541d07c5e6f9c405200331976d36346bd
Marco <marco.premi@mail.polimi.it>
2020-04-17 19:13:02 +0200

    [Servizi] - WIP

---
+++ website/js/inostriservizi.js
@@ -0,0 +1,15 @@
+window.onload = () => {
+    Breadcrumbs.loadCrumbs([
+        {
+            page: "homepage.html",
+            title: "Home"
+        },
+        {
+            page: "inostriservizi.html",
+            title: "I nostri servizi"
+        }
+    ]);
+    let form = document.getElementById("contact-form");
+    
+}
+

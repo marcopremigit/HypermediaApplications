ae8d157cdf75699af04ccf08903014480ada086e
Marco <marco.premi@mail.polimi.it>
2020-04-18 16:56:19 +0200

    Script removed

--- website/js/chisiamo.js
+++ website/js/chisiamo.js
@@ -12,25 +12,4 @@ window.onload = () => {
   }
 
 
- //*BannerImage 
-class BannerImage {
-    constructor(ref) {
-      this.hmi_ref = ref;
-      this.BS = {}
-      this.BS.image = document.createElement('img');    
-      this.BS.image.className = 'mt-2 mb-2 p-0 col-12 align-middle rounded';
-    }
-  
-    add (image){
-      this.BS.image.src = image;
-      let newNode = this.BS.image.cloneNode(true);
-      this.hmi_ref.appendChild(newNode);
-    }
-}
 
-const BannerImageSpace = document.getElementById('banner-image-space');
-let myBanner = new BannerImage( BannerImageSpace );
-myBanner.add('https://source.unsplash.com/random/1920x540');
- //*BannerImage 
-
- 

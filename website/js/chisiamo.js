744ba0ce2ab83e8f9a9d4bfde9c2c53f47227750
Marco <marco.premi@mail.polimi.it>
2020-04-18 16:26:34 +0200

    [BannerImage] - added from script

---
+++ website/js/chisiamo.js
@@ -0,0 +1,32 @@
+window.onload = () => {
+    Breadcrumbs.loadCrumbs([
+        {
+            page: "homepage.html",
+            title: "Home"
+        },
+        {
+            page: "chisiamo.html",
+            title: "Chi siamo"
+        }
+    ]);
+  }
+
+
+class BannerImage {
+    constructor(ref) {
+      this.hmi_ref = ref;
+      this.BS = {}
+      this.BS.image = document.createElement('img');    
+      this.BS.image.className = 'mt-2 p-0 col-12 align-middle rounded';
+    }
+  
+    add (image){
+      this.BS.image.src = image;
+      let newNode = this.BS.image.cloneNode(true);
+      this.hmi_ref.appendChild(newNode);
+    }
+}
+
+const BannerImageSpace = document.getElementById('banner-image-space');
+let myBanner = new BannerImage( BannerImageSpace );
+myBanner.add('https://source.unsplash.com/random/1920x540');

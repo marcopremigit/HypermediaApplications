49da283f4e0f522caa0a9a134e39e17f7e0ba1a9
Marco <marco.premi@mail.polimi.it>
2020-04-22 13:50:29 +0200

    [Template]] Completed

--- website/js/inostriservizi-detail.js
+++ website/js/inostriservizi-detail.js
@@ -15,6 +15,19 @@ function loadEvents(){
     ];
 }
 
+function loadVolunteers(){
+    //TODO: this has to be replaced with database query information extraction
+    return [
+        {id: 'ICSA', title: 'Giancarla Rossa', description: 'Ciao, sono Giancarla Rossa', img: 'https://source.unsplash.com/random/1920x1080', category: ["Vacanza studio"]},
+        {id: 'csaicas', title: 'Giancarla Bianca', description: 'Ciao, sono Giancarla Bianca', img: 'https://source.unsplash.com/random/1920x1080', category: ["Cena", "Vattelapesca"]},
+        {id: 'csaiodsa', title: 'Giancarla Nera', description: 'Ciao, sono Giancarla Nera', img: 'https://source.unsplash.com/random/1920x1080', category: ["Colletta"]},
+        {id: 'sadlsasdk', title: 'Giancarla Blu', description: 'Ciao, sono Giancarla Blu', img: 'https://source.unsplash.com/random/1920x1080', category: ["Vacanza studio"]},
+        {id: 'snakjoca', title: 'Giancarla Verde', description: 'Ciao, sono Giancarla Verde', img: 'https://source.unsplash.com/random/1920x1080', category: ["Colletta"]},
+        {id: 'fksaod', title: 'Giancarla Gialla', description: 'Ciao, sono Giancarla Gialla', img: 'https://source.unsplash.com/random/1920x1080', category: ["Colletta"]},
+        {id: 'kofgdap', title: 'Giancarla Rosa', description: 'Ciao, sono Giancarla Rosa', img: 'https://source.unsplash.com/random/1920x1080', category: ["Vacanza studio"]},
+    ];
+}
+
 function loadNextElement(goRight){
     let elementsOrder = sessionStorage.getItem('elementsOrder').split(',');
     let indexOfservice = elementsOrder.indexOf(service.id);
@@ -23,13 +36,9 @@ function loadNextElement(goRight){
 }
 
 function fillElements(){
-    document.getElementById('volunteerName').innerText = service.title;
-    document.getElementById('whoami').innerHTML = service.description;
-    document.getElementById('mycareer').innerHTML = service.description;
-    document.getElementById('mycontacts').innerHTML = service.description;
-    
-    document.getElementById('servicesTitle').innerHTML = `I servizi di ${service.title}`;
-    document.getElementById('eventsTitle').innerHTML = `I prossimi eventi di ${service.title}`;
+    document.getElementById('serviceName').innerText = service.title;
+    document.getElementById('serviceDescription').innerHTML = service.description;
+    document.getElementById('eventsTitle').innerHTML = `Eventi collegati`;
 }
 
 function loadService(id){
@@ -47,8 +56,8 @@ function loadService(id){
             title: "Home"
         },
         {
-            page: "inostrivolontari.html",
-            title: "I nostri volontari"
+            page: "inostriservizi.html",
+            title: "I nostri servizi"
         },
         {
             //TODO: mettere in title il nome del volontario
@@ -61,8 +70,11 @@ function loadService(id){
 
     //Query to database
     removeAllCards('card-space-events');
+    removeAllCards('card-space-volunteers');
     let events = loadEvents();
     loadCardsAndFilters(events, false,"", 'card-space-events');
+    let volunteers = loadVolunteers();
+    loadCardsAndFilters(volunteers,false,"inostrivolontari-detail.html",'card-space-volunteers')
 
     //Spinner handling
     Spinner.letThemComeBack();
--- website/js/inostriservizi.js
+++ website/js/inostriservizi.js
@@ -1,13 +1,13 @@
 function loadServices(){
     //TODO: this has to be replaced with database query information extraction
     return [
-        {id: "xdxd", title: 'Vacanza studio Londra', img: 'https://source.unsplash.com/random/1920x1080', link: 'https://source.unsplash.com/random/1920x1080', category: ["Vacanza studio"], lat: 45.468868, lng: 9.206720},
-        {id: "trpòpò", title: 'Cena di Natale', img: 'https://source.unsplash.com/random/1920x1080', link: 'https://source.unsplash.com/random/1920x1080', category: ["Cena", "Vattelapesca"], lat: 45.4507775, lng: 9.1709264},
-        {id: "ciaoo", title: 'Colletta Natalizia', img: 'https://source.unsplash.com/random/1920x1080', link: 'https://source.unsplash.com/random/1920x1080', category: ["Colletta"], lat: 45.4513128, lng: 9.1734067},
-        {id: "asfssada", title: 'Vacanza studio Catania', img: 'https://source.unsplash.com/random/1920x1080', link: 'https://source.unsplash.com/random/1920x1080', category: ["Vacanza studio"], lat: 45.4502473, lng: 9.1752055},
-        {id: "sassadas", title: 'Colletta alimentare', img: 'https://source.unsplash.com/random/1920x1080', link: 'https://source.unsplash.com/random/1920x1080', category: ["Colletta"], lat: 45.453261, lng: 9.179347},
-        {id: "dsdass", title: 'Colletta pasquale', img: 'https://source.unsplash.com/random/1920x1080', link: 'https://source.unsplash.com/random/1920x1080', category: ["Colletta"], lat: 45.486604, lng: 9.186649},
-        {id: "csdsad", title: 'Vacanza studio Trapani', img: 'https://source.unsplash.com/random/1920x1080', link: 'https://source.unsplash.com/random/1920x1080', category: ["Vacanza studio"], lat: 45.483227, lng: 9.188763},
+        {id: "xdxd", title: 'Vacanza studio Londra', description: "Vacanza studio Londa", img: 'https://source.unsplash.com/random/1920x1080', link: 'https://source.unsplash.com/random/1920x1080', category: ["Vacanza studio"], lat: 45.468868, lng: 9.206720},
+        {id: "trpòpò", title: 'Cena di Natale', description: "Cena di Natale", img: 'https://source.unsplash.com/random/1920x1080', link: 'https://source.unsplash.com/random/1920x1080', category: ["Cena", "Vattelapesca"], lat: 45.4507775, lng: 9.1709264},
+        {id: "ciaoo", title: 'Colletta Natalizia', description: "Colletta Natalizia", img: 'https://source.unsplash.com/random/1920x1080', link: 'https://source.unsplash.com/random/1920x1080', category: ["Colletta"], lat: 45.4513128, lng: 9.1734067},
+        {id: "asfssada", title: 'Vacanza studio Catania', description: "Vacanza studio Caatania", img: 'https://source.unsplash.com/random/1920x1080', link: 'https://source.unsplash.com/random/1920x1080', category: ["Vacanza studio"], lat: 45.4502473, lng: 9.1752055},
+        {id: "sassadas", title: 'Colletta alimentare', description: "Colletta alimentare", img: 'https://source.unsplash.com/random/1920x1080', link: 'https://source.unsplash.com/random/1920x1080', category: ["Colletta"], lat: 45.453261, lng: 9.179347},
+        {id: "dsdass", title: 'Colletta pasquale', description: "Colletta pasquale", img: 'https://source.unsplash.com/random/1920x1080', link: 'https://source.unsplash.com/random/1920x1080', category: ["Colletta"], lat: 45.486604, lng: 9.186649},
+        {id: "csdsad", title: 'Vacanza studio Trapani', description: "Vacanza studio Trapani", img: 'https://source.unsplash.com/random/1920x1080', link: 'https://source.unsplash.com/random/1920x1080', category: ["Vacanza studio"], lat: 45.483227, lng: 9.188763},
     ];
 }
 
@@ -34,7 +34,7 @@ window.onload = () => {
     })
     saveInStorage('services', servicesJSON, true);
     saveInStorage('elementsOrder', services.map(v => v.id));
-    
+
     //Shuffler handling
     window.demo = new Shuffler(document.querySelector('#card-space'), services.map(e => e.category));
     

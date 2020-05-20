1dbf8b74e612a8bce8ca6376e3d0c284fd5fef50
DavideCl <davide2.clementi@mail.polimi.it>
2020-05-20 11:51:03 +0200

    [Contattaci] Send Email removed temporarily

--- website/public/assets/js/contattaci.js
+++ website/public/assets/js/contattaci.js
@@ -22,33 +22,6 @@ $(document).ready(() => {
     }, false); 
 });
 
-function sendEmail() {
-    let value = document.getElementById('contact-form'); 
-    var email =  value.querySelector("#email").value; 
-    var subject = value.querySelector("#subject").value; 
-    var message = value.querySelector("#message").value; 
-	Email.send({
-	Host: "smtp.gmail.com",
-	Username : "ripe4u4@gmail.com",
-	Password : "ripe4hypermedia",
-	To : 'ripe4u4@gmail.com',
-	From : email,
-	Subject : subject,
-	Body : message,
-    }).then(m => {
-        if(m==="OK"){
-            document.getElementById("contact-form").style.display="none";
-            document.getElementById("alert-success").style.display="block";   
-        }else{
-            document.getElementById("contact-form").style.display="none";
-            document.getElementById("alert-danger").style.display="block";
-        }
-        
-       
-    }		
-	);
-}
-
 function writeAnotherEmail(){
     document.getElementById("subject").value='';
     document.getElementById("message").value='';

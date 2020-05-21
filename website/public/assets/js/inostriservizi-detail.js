a29273646ae9320725e55cf8c921285216d2a5b4
Marco <marco.premi@mail.polimi.it>
2020-05-21 14:26:25 +0200

    [Deploy] - Service volunteers

--- website/public/assets/js/inostriservizi-detail.js
+++ website/public/assets/js/inostriservizi-detail.js
@@ -5,27 +5,27 @@ $(document).ready(() => {
     loadService(window.location.href.split('id=')[1]);
 });
 
-function loadEvents(){
-    //TODO: this has to be replaced with database query information extraction
-    return [
-        {id: 'ICSA', title: 'Vacanza studio Londra', img: 'https://source.unsplash.com/random/1920x1080', category: ["Vacanza studio"]},
-        {id: 'csaicas', title: 'Cena di Natale', img: 'https://source.unsplash.com/random/1920x1080', category: ["Cena", "Vattelapesca"]},
-        {id: 'csaiodsa', title: 'Colletta Natalizia', img: 'https://source.unsplash.com/random/1920x1080', category: ["Colletta"]},
-    ];
-}
+// function loadEvents(){
+//     //TODO: this has to be replaced with database query information extraction
+//     return [
+//         {id: 'ICSA', title: 'Vacanza studio Londra', img: 'https://source.unsplash.com/random/1920x1080', category: ["Vacanza studio"]},
+//         {id: 'csaicas', title: 'Cena di Natale', img: 'https://source.unsplash.com/random/1920x1080', category: ["Cena", "Vattelapesca"]},
+//         {id: 'csaiodsa', title: 'Colletta Natalizia', img: 'https://source.unsplash.com/random/1920x1080', category: ["Colletta"]},
+//     ];
+// }
 
-function loadVolunteers(){
-    //TODO: this has to be replaced with database query information extraction
-    return [
-        {id: 'ICSA', title: 'Giancarla Rossa', description: 'Ciao, sono Giancarla Rossa', img: 'https://source.unsplash.com/random/1920x1080', category: ["Vacanza studio"]},
-        {id: 'csaicas', title: 'Giancarla Bianca', description: 'Ciao, sono Giancarla Bianca', img: 'https://source.unsplash.com/random/1920x1080', category: ["Cena", "Vattelapesca"]},
-        {id: 'csaiodsa', title: 'Giancarla Nera', description: 'Ciao, sono Giancarla Nera', img: 'https://source.unsplash.com/random/1920x1080', category: ["Colletta"]},
-        {id: 'sadlsasdk', title: 'Giancarla Blu', description: 'Ciao, sono Giancarla Blu', img: 'https://source.unsplash.com/random/1920x1080', category: ["Vacanza studio"]},
-        {id: 'snakjoca', title: 'Giancarla Verde', description: 'Ciao, sono Giancarla Verde', img: 'https://source.unsplash.com/random/1920x1080', category: ["Colletta"]},
-        {id: 'fksaod', title: 'Giancarla Gialla', description: 'Ciao, sono Giancarla Gialla', img: 'https://source.unsplash.com/random/1920x1080', category: ["Colletta"]},
-        {id: 'kofgdap', title: 'Giancarla Rosa', description: 'Ciao, sono Giancarla Rosa', img: 'https://source.unsplash.com/random/1920x1080', category: ["Vacanza studio"]},
-    ];
-}
+// function loadVolunteers(){
+//     //TODO: this has to be replaced with database query information extraction
+//     return [
+//         {id: 'ICSA', title: 'Giancarla Rossa', description: 'Ciao, sono Giancarla Rossa', img: 'https://source.unsplash.com/random/1920x1080', category: ["Vacanza studio"]},
+//         {id: 'csaicas', title: 'Giancarla Bianca', description: 'Ciao, sono Giancarla Bianca', img: 'https://source.unsplash.com/random/1920x1080', category: ["Cena", "Vattelapesca"]},
+//         {id: 'csaiodsa', title: 'Giancarla Nera', description: 'Ciao, sono Giancarla Nera', img: 'https://source.unsplash.com/random/1920x1080', category: ["Colletta"]},
+//         {id: 'sadlsasdk', title: 'Giancarla Blu', description: 'Ciao, sono Giancarla Blu', img: 'https://source.unsplash.com/random/1920x1080', category: ["Vacanza studio"]},
+//         {id: 'snakjoca', title: 'Giancarla Verde', description: 'Ciao, sono Giancarla Verde', img: 'https://source.unsplash.com/random/1920x1080', category: ["Colletta"]},
+//         {id: 'fksaod', title: 'Giancarla Gialla', description: 'Ciao, sono Giancarla Gialla', img: 'https://source.unsplash.com/random/1920x1080', category: ["Colletta"]},
+//         {id: 'kofgdap', title: 'Giancarla Rosa', description: 'Ciao, sono Giancarla Rosa', img: 'https://source.unsplash.com/random/1920x1080', category: ["Vacanza studio"]},
+//     ];
+// }
 
 function loadNextElement(goRight){
     let elementsOrder = sessionStorage.getItem('servicesElementsOrder').split(',');
@@ -68,26 +68,44 @@ function loadService(id){
     fillElements();
 
     //Query to database
-    removeAllCards('events-card-space');
-    removeAllCards('volunteers-card-space');
-    let events = loadEvents();
-    loadCardsAndFilters(events, false,"inostrieventi-detail.html", '#events-card-space');
-    let eventsJSON = {};
-    events.map(e=>{
-        eventsJSON[e.id] = e;
-    })
-    saveInStorage('events',eventsJSON);
-    saveInStorage('eventsElementsOrder', events.map(v => v.id));
+    // removeAllCards('events-card-space');
+    // removeAllCards('volunteers-card-space');
+    // let events = loadEvents();
+    // loadCardsAndFilters(events, false,"inostrieventi-detail.html", '#events-card-space');
+    // let eventsJSON = {};
+    // events.map(e=>{
+    //     eventsJSON[e.id] = e;
+    // })
+    // saveInStorage('events',eventsJSON);
+    // saveInStorage('eventsElementsOrder', events.map(v => v.id));
 
-    let volunteers = loadVolunteers();
-    loadCardsAndFilters(volunteers, false, "inostrivolontari-detail.html", '#volunteers-card-space');
-    let volunteersJSON = {};
-    volunteers.map(e=>{
-        volunteersJSON[e.id] = e;
+    loadVolunteerServices(id)
+    .then(volunteers => {
+        loadCardsAndFilters(volunteers, false, "inostrivolontari-detail.html", '#volunteers-card-space');
+        let volunteersJSON = {};
+        volunteers.map(e=>{
+            volunteersJSON[e.id] = e;
+        })
+        saveInStorage('volunteers',volunteersJSON);
+        saveInStorage('volunteersElementsOrder', volunteers.map(v => v.id));
+    
+        //Spinner handling
+        Spinner.letThemComeBack();
     })
-    saveInStorage('volunteers',volunteersJSON);
-    saveInStorage('volunteersElementsOrder', volunteers.map(v => v.id));
+    
+}
 
-    //Spinner handling
-    Spinner.letThemComeBack();
+async function loadVolunteerServices(id_service){
+    return await $.getJSON(DB_URL + "/volunteer_service",
+    {
+        id_service: id_service
+    },
+    (data, status) => {
+        if(status === "success"){
+            console.log(data);
+            return data;
+        }
+        else
+            console.error(status);
+    });
 }

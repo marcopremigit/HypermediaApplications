a160ce6a21fe4ada7b689772e52e604bbc8745a6
Fabs <fabrizio.siciliano@mail.polimi.it>
2020-05-25 15:40:26 +0200

    [Testing] - Volunteers GET returning 404 WIP

--- website/other/server/controllers/Volunteers.js
+++ website/other/server/controllers/Volunteers.js
@@ -36,10 +36,8 @@ module.exports.volunteersGET = function volunteersGET (req, res, next, category,
 module.exports.volunteersVolunteerIdGET = function volunteersVolunteerIdGET (req, res, next, volunteerId) {
   Volunteers.volunteersVolunteerIdGET(volunteerId)
     .then(function (response) {
-      console.log('[VOLUNTEERS GET] - RESPONSE:');
-      console.log(res);
-      console.log(response);
-      utils.writeJson(res, response);
+      if(Object.keys(response).length > 0) utils.writeJson(res, response);
+      else utils.respondWithCode(404, 'Diobestia');
     })
     .catch(function (response) {
       utils.writeJson(res, response);
--- website/other/server/service/JobsService.js
+++ website/other/server/service/JobsService.js
@@ -29,38 +29,6 @@ exports.jobsDbSetup = function(s) {
  * limit Integer the limit of objects to return (optional)
  * offset Integer the offset of the objects to retur (optional)
  * returns List
- **
-exports.jobsGET = function(limit,offset) {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = [ {
-  "requirements" : "requirements",
-  "description" : "description",
-  "closed" : true,
-  "id" : 0,
-  "title" : "title"
-}, {
-  "requirements" : "requirements",
-  "description" : "description",
-  "closed" : true,
-  "id" : 0,
-  "title" : "title"
-} ];
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
-  });
-}
-*/
-
-/**
- * Returns all the jobs in the database
- *
- * limit Integer the limit of objects to return (optional)
- * offset Integer the offset of the objects to retur (optional)
- * returns List
  **/
 exports.jobsGET = function(limit,offset) {
   if(!limit) limit = 10;

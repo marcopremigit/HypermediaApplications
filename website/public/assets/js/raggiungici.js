37a4c58ec1b6431935390c6395ea3bd01d0227b2
Fabs <fabrizio.siciliano@mail.polimi.it>
2020-05-29 11:52:40 +0200

    [Raggiungici] - Fixed bug

--- website/public/assets/js/raggiungici.js
+++ website/public/assets/js/raggiungici.js
@@ -1,8 +1,8 @@
-function initMap() {
+$(document).ready(() => {
     let coordinates = {lat: 45.468868, lng: 9.206720};
     let map = new google.maps.Map(document.getElementById('map'), {
         center: coordinates,
         zoom: 18
     });
     new google.maps.Marker({position: coordinates, map: map});
-}
+});

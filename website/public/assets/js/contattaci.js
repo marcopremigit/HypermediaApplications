8e1bbf5bc7f511960634eda3e406c1851cdc2739
Marco <marco.premi@mail.polimi.it>
2020-05-29 11:30:42 +0200

    [ContactForm] - updated

--- website/public/assets/js/contattaci.js
+++ website/public/assets/js/contattaci.js
@@ -1,5 +1,15 @@
-$(document).ready(() => {
-    let form = $("#contact-form");
+window.onload = () => {
+    Breadcrumbs.loadCrumbs([
+        {
+            page: "homepage.html",
+            title: "Home"
+        },
+        {
+            page: "contattaci.html",
+            title: "Contattaci"
+        }
+    ]);
+    let form = document.getElementById("contact-form");
 
     form.addEventListener('submit', function(event) {
         if (form.checkValidity() === false) {
@@ -10,11 +20,23 @@ $(document).ready(() => {
         }
         form.classList.add('was-validated');
     }, false); 
-});
+    
+}
+
+
+function sendEmail() {
+    document.getElementById("contact-form").style.display="none";
+    document.getElementById("alert-success").style.display="block";  
+
+}
 
 function writeAnotherEmail(){
-    $("#subject").value='';
-    $("#message").value='';
-    $("#alert-success").style.display="none";
-    $("#contact-form").style.display="block";
+    document.getElementById("subject").value='';
+    document.getElementById("message").value='';
+    document.getElementById("alert-success").style.display="none";
+    document.getElementById("contact-form").style.display="block";
+
 }
+
+
+
